dfb292c56fd804ce9c4b4276f449bd6e309e6970
Eziliun <wilianw2011@hotmail.com>
2024-10-27 17:13:56 -0300

    feat: Seção de Sedes e seus serviços

--- src/app/Pages/gym-page/components/gym-edit-dialog/gym-edit-dialog.component.ts
+++ src/app/Pages/gym-page/components/gym-edit-dialog/gym-edit-dialog.component.ts
@@ -1,17 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { AcademiaService } from '../../../../Shared/Service/Academia.service';
 
 @Component({
   selector: 'app-gym-edit-dialog',
   standalone: true,
-  imports: [InputTextModule, ButtonModule],
+  imports: [InputTextModule, ButtonModule, OverlayPanelModule],
   templateUrl: './gym-edit-dialog.component.html',
-  styleUrl: './gym-edit-dialog.component.scss'
+  styleUrls: ['./gym-edit-dialog.component.scss']
 })
-export class GymEditDialogComponent {
-  cnpj: string = '';
-  email: string = '';
-  lotacaoMedia: number | null = null;
+export class GymEditDialogComponent implements OnInit {
+  academiaForm: FormGroup;
+  sedeForm: FormGroup;
   quantidadeSedes: number = 3;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private academiaService: AcademiaService,
+  ) {
+    this.academiaForm = this.formBuilder.group({
+      cnpj: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      lotacaoMedia: [null, Validators.required]
+    });
+
+    this.sedeForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      endereco: ['', Validators.required],
+      lotacao: [null, Validators.required]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  saveAcademia(): void {
+    if (this.academiaForm.invalid) {
+      return;
+    }
+
+    const payload = this.academiaForm.value;
+
+    this.academiaService.editAcademia(payload.cnpj, payload).subscribe(response => {
+      console.log('Academia editada com sucesso', response);
+    });
+  }
+
+  addSede(): void {
+    if (this.sedeForm.invalid) {
+      return;
+    }
+
+    const payload = this.sedeForm.value;
+
+    this.academiaService.addSede(this.academiaForm.value.cnpj, payload).subscribe(response => {
+      console.log('Sede adicionada com sucesso', response);
+      this.sedeForm.reset();
+    });
+  }
 }
--- src/app/Shared/Service/Academia.service.ts
+++ src/app/Shared/Service/Academia.service.ts
@@ -18,4 +18,16 @@ export class AcademiaService {
   editAcademia(cnpj: string, payload: any): Observable<any> {
     return this.http.put<any>(`${this.apiUrl}/update_acad/${cnpj}`, payload);
   }
+
+  addSede(cnpj: string, payload: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/add_sede/${cnpj}`, payload);
+  }
+  
+  editSede(cnpj: string, sedeId: number, payload: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/edit_sede/${cnpj}/${sedeId}`, payload);
+  }
+
+  getSedes(cnpj: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/get_sedes/${cnpj}`);
+  }
 }
